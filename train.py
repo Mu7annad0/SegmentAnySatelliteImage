9004eb6d9d59391c7ccab7ad41ea1badb0acca96
Muhannad Ashraf <mu7nnd.as@gmail.com>
2024-04-21 19:38:34 +0200

    SASI

---
+++ train.py
@@ -0,0 +1,17 @@
+from ultralytics import YOLO
+
+if __name__ == "__main__":
+
+    model = YOLO(model="ck/yolov8x-seg.pt")
+    model.train(data="Dataset/yolo_dataset/data.yaml",
+                epochs=100,
+                batch=16,
+                imgsz=512,
+                overlap_mask=True,
+                save=True,
+                save_period=5,
+                device='mps',
+                project='results',
+                name='test',
+                val=False,
+                patience = 5)
